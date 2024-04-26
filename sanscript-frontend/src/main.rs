0e34294ed4c2c92d3aca0d935ea7a4afcac328fd
StefanJo3107 <sjovanovic0831@gmail.com>
2024-04-26 03:26:14 +0200

    Serializing compiled bytecode to postcard file

--- sanscript-frontend/src/main.rs
+++ sanscript-frontend/src/main.rs
@@ -1,19 +1,5 @@
-use sanscript_common::{
-    chunk::{Chunk, OpCode},
-};
-use sanscript_common::value::Value;
+use sanscript_frontend::runner::run;
 
 fn main() {
-    let mut chunk = Chunk::new();
-
-    let mut const_offset = chunk.add_constant(Value::ValNumber(4.2));
-    chunk.write_chunk(OpCode::OpConstant(const_offset), 123);
-    const_offset = chunk.add_constant(Value::ValNumber(2.4));
-    chunk.write_chunk(OpCode::OpConstant(const_offset), 124);
-    chunk.write_chunk(OpCode::OpNegate, 124);
-    chunk.write_chunk(OpCode::OpSubtract, 125);
-    chunk.write_chunk(OpCode::OpReturn, 126);
-    // let mut vm = VM::new(&chunk);
-    // vm.interpret("TEST CHUNK");
-    // disassemble_chunk(&chunk, "TEST CHUNK");
+    run();
 }
--- sanscript-frontend/src/runner.rs
+++ sanscript-frontend/src/runner.rs
@@ -3,44 +3,37 @@ use std::fs::File;
 use std::io;
 use std::io::{Read, Write};
 use std::process::exit;
+use postcard::to_allocvec;
+use sanscript_common::value::FunctionType;
+use crate::compiler::Compiler;
 
 pub fn run() {
-    if env::args().len() == 1 {
-        if let Err(e) = repl() {
-            eprintln!("{}", e.to_string());
-            exit(1);
-        }
-    } else if env::args().len() == 2 {
-        if let Err(e) = run_file(env::args().last().unwrap().as_str()) {
+    if env::args().len() == 3 {
+        let args: Vec<String> = env::args().collect();
+        if let Err(e) = run_file(args[1].as_str(), args[2].as_str()) {
             eprintln!("{}", e.to_string());
             exit(1);
         }
     } else {
-        eprintln!("Usage: SanScript [path]");
+        eprintln!("Usage: SanScript [source path] [destination path]");
         exit(1);
     }
 }
 
-pub fn repl() -> io::Result<()> {
-    let stdin = io::stdin();
-
-    loop {
-        print!("> ");
-        io::stdout().flush()?;
-        let mut input = String::new();
-        _ = stdin.read_line(&mut input);
-    }
-}
-
-pub fn run_file(path: &str) -> io::Result<()> {
-    read_file(path)?;
+pub fn run_file(source_path: &str, dest_path: &str) -> io::Result<()> {
+    read_file(source_path, dest_path);
     Ok(())
 }
 
-fn read_file(path: &str) -> io::Result<String> {
-    let mut source_file = File::open(path)?;
+fn read_file(source_path: &str, dest_path: &str) {
+    let mut source_file = File::open(source_path).unwrap_or_else(|e|{panic!("Error opening file at path {}: {}", source_path, e.to_string())});
     let mut source: String = String::from("");
-    source_file.read_to_string(&mut source)?;
-
-    Ok(source)
+    source_file.read_to_string(&mut source).unwrap_or_else(|e|{panic!("Error reading file content: {}", e.to_string())});
+    println!("{}", source);
+    let mut compiler = Compiler::new(source.as_str(), FunctionType::Script);
+    if let Some(function) = compiler.compile() {
+        let output = to_allocvec(&function).unwrap_or_else(|e|{panic!("Error serializing compiler result: {}", e)});
+        let mut file = File::create(dest_path).unwrap_or_else(|e|{panic!("Error opening file at path {}: {}", dest_path, e.to_string())});
+        file.write_all(output.as_slice()).expect("Error writing serialized data to a file");
+    }
 }
