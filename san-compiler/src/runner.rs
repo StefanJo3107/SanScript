13a9c7766ac629c546eaffdd176cbb71f0207025
StefanJo3107 <sjovanovic0831@gmail.com>
2024-12-31 23:25:30 +0100

    Added success and fail messages

--- san-compiler/src/runner.rs
+++ san-compiler/src/runner.rs
@@ -29,11 +29,13 @@ fn read_file(source_path: &str, dest_path: &str) {
     let mut source_file = File::open(source_path).unwrap_or_else(|e|{panic!("Error opening file at path {}: {}", source_path, e.to_string())});
     let mut source: String = String::from("");
     source_file.read_to_string(&mut source).unwrap_or_else(|e|{panic!("Error reading file content: {}", e.to_string())});
-    println!("{}", source);
     let mut compiler = Compiler::new(source.as_str(), FunctionType::Script);
     if let Some(function) = compiler.compile() {
         let output = to_allocvec(&function).unwrap_or_else(|e|{panic!("Error serializing compiler result: {}", e)});
         let mut file = File::create(dest_path).unwrap_or_else(|e|{panic!("Error opening file at path {}: {}", dest_path, e.to_string())});
         file.write_all(output.as_slice()).expect("Error writing serialized data to a file");
+        println!("Code compiled successfully!");
+    } else {
+        println!("Code failed to compile!");
     }
 }
